cb68a9ae67792530074e98d82dc2daee66bb5073
thinhxd12 <thinhxd12@gmail.com>
2021-07-19 23:24:06 +0700

    v2

--- src/BaiTapThuKinh/BaiTapThuKinh.js
+++ src/BaiTapThuKinh/BaiTapThuKinh.js
@@ -70,6 +70,12 @@ export default class BaiTapThuKinh extends Component {
     ]
 
     state = {
+        glassesSrc: "",
+        glassesName: "",
+        glassesDesc: ""
+    }
+
+    compare = {
         glassesSrc: "./glassesImage/v1.png",
         glassesName: "GUCCI G8850U",
         glassesDesc: "Light pink square lenses define these sunglasses, ending with amother of pearl effect tip. "
@@ -78,18 +84,36 @@ export default class BaiTapThuKinh extends Component {
     renderTable = () => {
         let content = this.dataGlasses.map((glasses, index) => {
             return <div key={index} className="col-md-2">
-                <div className="card mb-3">
-                    <button className="btn btn-light">
-                        <img src={glasses.url} className="img-fluid" alt="..." onClick={() => {
-                            this.changeGlasses(`${glasses.url}`, `${glasses.name}`, `${glasses.desc}`);
-                        }} />
-
+                <div className="mb-3">
+                    <button className="btn-default card__img" onClick={() => {
+                        this.changeGlasses(`${glasses.url}`, `${glasses.name}`, `${glasses.desc}`);
+                    }} >
+                        <img src={glasses.url} className="img-fluid" alt="..." />
                     </button>
+
                 </div>
             </div>
 
         });
         return content;
+    }
+
+    renderModel = () => {
+        if (this.state.glassesSrc !== "") {
+            return <div className="card w-25">
+                <div className="model">
+                    <img src="./glassesImage/model.jpg" alt="..." className="img-fluid" />
+                    <img src={this.state.glassesSrc} alt="..." className="img-fluid glasses__model" />
+                </div>
+                <div className="glasses__info">
+                    <h4>{this.state.glassesName}</h4>
+                    <p>{this.state.glassesDesc}</p>
+                </div>
+            </div>
+        }
+        else return <div className="card w-25">
+            <img src="./glassesImage/model.jpg" alt="..." className="img-fluid" />
+        </div>
     }
 
     changeGlasses = (url, name, desc) => {
@@ -104,27 +128,25 @@ export default class BaiTapThuKinh extends Component {
         return (
             <div className="glasses__app" style={{ background: 'url(./glassesImage/background.jpg)' }}>
                 <h3>TRY GLASS APP ONLINE</h3>
-                <div className="container py-5 w-75">
+                <div className="container pt-3 pb-5 w-75">
 
                     <div className="mx-auto mb-3 d-flex justify-content-around">
                         <div className="card w-25">
                             <div className="model">
                                 <img src="./glassesImage/model.jpg" alt="..." className="img-fluid" />
-                                <img src={this.state.glassesSrc} alt="..." className="img-fluid glasses__model" />
+                                <img src={this.compare.glassesSrc} alt="..." className="img-fluid glasses__model" />
                             </div>
                             <div className="glasses__info">
-                                <h4>{this.state.glassesName}</h4>
-                                <p>{this.state.glassesDesc}</p>
+                                <h4>{this.compare.glassesName}</h4>
+                                <p>{this.compare.glassesDesc}</p>
                             </div>
                         </div>
 
-                        <div className="card w-25">
-                            <img src="./glassesImage/model.jpg" alt="..." className="img-fluid" />
-                        </div>
+                        {this.renderModel()}
 
                     </div>
 
-                    <div className="card pt-3 px-3">
+                    <div className="card__item p-3">
                         <div className="row">
                             {this.renderTable()}
 
